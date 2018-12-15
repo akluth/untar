d9ce47d841c2986a5c6982d881a8004052f92a19
Alexander Kluth <deralex@cpan.org>
2018-12-16 00:32:14 +0100

    Apply changes suggested by 'cargo fmt'
    Signed-off-by: Alexander Kluth <deralex@cpan.org>

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,30 @@
-extern crate tar;
 extern crate clap;
-extern crate dialoguer;
 extern crate colored;
+extern crate dialoguer;
+extern crate tar;
 
-use clap::{Arg, App};
+use clap::{App, Arg};
 use colored::*;
 use dialoguer::Confirmation;
-use std::io::prelude::*;
-use std::io::{self, Read};
+use libflate::gzip::Decoder;
 use std::fs;
 use std::fs::File;
+use std::io::prelude::*;
+use std::io::{self, Read};
 use std::path::Path;
 use tar::Archive;
-use libflate::gzip::Decoder;
 
 fn main() -> io::Result<()> {
     let matches = App::new("untar")
         .version("0.1.0")
         .author("Alexander Kluth <deralex@cpan.org>")
         .about("Unpack tar archives with various compression algorithms (gz, bzip2 etc.)")
-        .arg(Arg::with_name("FILE")
-            .help("tar archive to unpack")
-            .required(true)
-            .index(1))
+        .arg(
+            Arg::with_name("FILE")
+                .help("tar archive to unpack")
+                .required(true)
+                .index(1),
+        )
         .get_matches();
 
     let file = File::open(matches.value_of("FILE").unwrap()).unwrap();
@@ -33,7 +35,11 @@ fn main() -> io::Result<()> {
     for file in a.entries().unwrap() {
         let mut file = file.unwrap();
 
-        println!("{} {:?}", "Extracting".green().bold(), file.header().path().unwrap());
+        println!(
+            "{} {:?}",
+            "Extracting".green().bold(),
+            file.header().path().unwrap()
+        );
 
         let path = file.path()?;
 
@@ -64,7 +70,7 @@ fn main() -> io::Result<()> {
         } else {
             file_to_write = File::create(file.header().path().unwrap())?;
             data = Vec::new();
-            
+
             file.read_to_end(&mut data).unwrap();
             file_to_write.write_all(&data)?;
         }
